1f4bb1c4457b7b061b2bb34b444539326edb5f8b
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-10-05 00:38:24 -0700

    chore: update fermi

--- snippets/fermi.rs
+++ snippets/fermi.rs
@@ -7,15 +7,12 @@ static COUNT: Atom<i32> = |_| 0;
 fn Demo(cx: Scope) -> Element {
     let count = use_read(&cx, || COUNT);
 
-    cx.render(rsx! {
-        "Count: {count}"
-        Increment {}
-    })
+    render!("Count: {count}")
 }
 
 // Or write to it from anywhere
 fn Increment(cx: Scope) -> Element {
     let mut count = use_atom(&cx, COUNT);
 
-    cx.render(rsx! { button { onclick: move |_| count += 1, "Increment" } })
+    render!(button { onclick: move |_| count += 1, "Increment" } )
 }

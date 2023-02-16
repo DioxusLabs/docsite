8aed5f2df4af52cd9b0ce93a834c13d990546e41
jkelleyrtp <jkelleyrtp@gmail.com>
2023-02-16 23:14:26 +0000

    Deploying to gh-pages from @ DioxusLabs/docsite@1e090526a2b717ad18cd4682e9c002662699c428 🚀

--- assets/dioxus/snippets/dioxus-interpreter-js-1676574062e4c953/inline0.js
+++ assets/dioxus/snippets/dioxus-interpreter-js-1676574062e4c953/inline0.js
@@ -1,4 +1,4 @@
-let m,p,ls,lss,sp,d,t,c,s,sl,op,i,e,z,event_name,n,ns,many,root,value,index,tmpl_id,len,text,ptr,field,id,bubbles;const attr = [];const ns_cache = [];const evt = [];
+let m,p,ls,lss,sp,d,t,c,s,sl,op,i,e,z,value,n,len,bubbles,event_name,ptr,tmpl_id,text,many,ns,field,index,root,id;const evt = [];const attr = [];const ns_cache = [];
     class ListenerMap {
         constructor(root) {
             // bubbling events can listen at the root element

c2c1bfa93457ae4c4c4170a1bd76602ecfdaa191
Evan Almloff <evanalmloff@gmail.com>
2023-07-18 16:04:03 -0700

    create syntect_html_fs macro

--- syntect-html/src/lib.rs
+++ syntect-html/src/lib.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
@@ -10,9 +12,7 @@ use syntect::highlighting::ThemeSet;
 use syntect::parsing::SyntaxSet;
 
 struct CodeBlock {
-    extension: String,
-    code: String,
-    theme: String,
+    html: String,
 }
 
 impl Parse for CodeBlock {
@@ -26,11 +26,100 @@ impl Parse for CodeBlock {
         let theme = input
             .parse::<LitStr>()
             .unwrap_or_else(|_| LitStr::new("base16-ocean.dark", Span::call_site()));
-        Ok(CodeBlock {
-            extension: extension.value(),
-            code: code.value(),
-            theme: theme.value(),
+
+        Self::new(code.value(), extension.value(), theme.value())
+    }
+}
+
+impl CodeBlock {
+    fn new(code: String, extension: String, theme: String) -> syn::Result<Self> {
+        let ss = SyntaxSet::load_defaults_newlines();
+        let ts = ThemeSet::load_defaults();
+
+        let maybe_theme = ts.themes.get(&theme);
+        let theme = maybe_theme.ok_or_else(|| {
+            syn::Error::new(Span::call_site(), format!("No theme found for {}", theme))
+        })?;
+        let syntax = ss.find_syntax_by_extension(&extension).ok_or_else(|| {
+            syn::Error::new(
+                Span::call_site(),
+                format!("No syntax found for extension {}", extension),
+            )
+        })?;
+        let html = syntect::html::highlighted_html_for_string(&code, &ss, &syntax, theme).map_err(
+            |err| {
+                syn::Error::new(
+                    Span::call_site(),
+                    format!("Error while generating HTML: {}", err),
+                )
+            },
+        )?;
+        Ok(CodeBlock { html })
+    }
+}
+
+impl ToTokens for CodeBlock {
+    fn to_tokens(&self, tokens: &mut proc_macro2::TokenStream) {
+        let html = &self.html;
+
+        tokens.extend(quote! {
+            #html
         })
+    }
+}
+
+struct CodeBlockFs {
+    html: String,
+}
+
+impl Parse for CodeBlockFs {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let code_path = input.parse::<LitStr>()?;
+        let code_path = code_path.value();
+        let _ = input.parse::<Token![,]>();
+        let theme = input
+            .parse::<LitStr>()
+            .unwrap_or_else(|_| LitStr::new("base16-ocean.dark", Span::call_site()));
+        let theme = theme.value();
+
+        let path  = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").map_err(|_|{
+            syn::Error::new(Span::call_site(), "CARGO_MANIFEST_DIR not found")
+        })?);
+        let path = path.join(&code_path);
+        let code = std::fs::read_to_string(&path).map_err(|err|{
+            syn::Error::new(Span::call_site(), format!("Error while reading file: {} while reading {}", err, path.display()))
+        })?;
+        let extension = path.extension();
+        let extension = &*extension.unwrap().to_string_lossy();
+        let html = syntect::html::highlighted_html_for_string(
+            &code,
+            &syntect::parsing::SyntaxSet::load_defaults_newlines(),
+            &syntect::parsing::SyntaxSet::load_defaults_newlines()
+                .find_syntax_by_extension(extension)
+                .unwrap(),
+            &syntect::highlighting::ThemeSet::load_defaults()
+                .themes
+                .get(&theme)
+                .unwrap(),
+        )
+        .unwrap();
+
+        Ok(CodeBlockFs { html })
+    }
+}
+
+/// Generate a HTML string from a code block path.
+#[proc_macro]
+pub fn syntect_html_fs(input: TokenStream) -> TokenStream {
+    match syn::parse::<CodeBlockFs>(input) {
+        Ok(block) => {
+            let html = &block.html;
+            quote! {
+                #html
+            }
+            .into()
+        }
+        Err(err) => err.to_compile_error().into(),
     }
 }
 
@@ -48,45 +137,15 @@ impl Parse for CodeBlock {
 ///     }
 ///     "#,
 ///     "rs",
-///     "base16-ocean.dark",
+///     "base16-ocean.dark"
 /// };
 #[proc_macro]
 pub fn syntect_html(input: TokenStream) -> TokenStream {
     match syn::parse::<CodeBlock>(input) {
-        Ok(block) => {
-            let ss = SyntaxSet::load_defaults_newlines();
-            let ts = ThemeSet::load_defaults();
-
-            let theme = ts.themes.get(&block.theme);
-            let theme = match theme.ok_or_else(|| {
-                syn::Error::new(
-                    Span::call_site(),
-                    format!("No theme found for {}", block.theme),
-                )
-            }) {
-                Ok(theme) => theme,
-                Err(err) => return err.to_compile_error().into(),
-            };
-            let syntax = match ss
-                .find_syntax_by_extension(&block.extension)
-                .ok_or_else(|| {
-                    syn::Error::new(
-                        Span::call_site(),
-                        format!("No syntax found for extension {}", block.extension),
-                    )
-                }) {
-                Ok(syntax) => syntax,
-                Err(err) => return err.to_compile_error().into(),
-            };
-            let html = syntect::html::highlighted_html_for_string(&block.code, &ss, &syntax, theme)
-                .unwrap();
-
-            quote! {
-                #html
-            }
-            .into_token_stream()
-            .into()
+        Ok(block) => quote! {
+            #block
         }
+        .into(),
         Err(err) => err.to_compile_error().into(),
     }
 }

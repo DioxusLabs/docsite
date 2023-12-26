9fe17717de84e001743b3629c632bb520424cd0f
Evan Almloff <evanalmloff@gmail.com>
2023-12-26 10:12:50 -0600

    fix formatting

--- src/doc_examples/mod.rs
+++ src/doc_examples/mod.rs
@@ -30,15 +30,15 @@ pub mod hackernews_post;
 #[cfg(not(feature = "doc_test"))]
 pub mod hackernews_state;
 #[cfg(not(feature = "doc_test"))]
-pub mod hooks_out_of_date;
-#[cfg(not(feature = "doc_test"))]
-pub mod hooks_use_ref;
+pub mod hello_world;
 #[cfg(not(feature = "doc_test"))]
 pub mod hooks_counter;
 #[cfg(not(feature = "doc_test"))]
-pub mod rsx_overview;
-#[cfg(not(feature = "doc_test"))]
 pub mod hooks_counter_two_state;
+#[cfg(not(feature = "doc_test"))]
+pub mod hooks_out_of_date;
+#[cfg(not(feature = "doc_test"))]
+pub mod hooks_use_ref;
 #[cfg(not(feature = "doc_test"))]
 pub mod input_controlled;
 #[cfg(not(feature = "doc_test"))]
@@ -48,13 +48,12 @@ pub mod readme;
 #[cfg(not(feature = "doc_test"))]
 pub mod rendering_lists;
 #[cfg(not(feature = "doc_test"))]
+pub mod rsx_overview;
+#[cfg(not(feature = "doc_test"))]
 pub mod spawn;
 #[cfg(not(feature = "doc_test"))]
 pub mod use_future;
-#[cfg(not(feature = "doc_test"))]
-pub mod hello_world;
 
 // Check any examples we don't compile into the docs
 #[cfg(feature = "doc_test")]
 automod::dir!(pub "src/doc_examples");
-

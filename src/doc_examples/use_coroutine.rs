8dc2a9fc204aa90975a13e9263970ea4d8355037
marc2332 <mespinsanz@gmail.com>
2024-04-05 18:15:25 +0200

    simplify

--- src/doc_examples/use_coroutine.rs
+++ src/doc_examples/use_coroutine.rs
@@ -7,33 +7,31 @@ pub fn App() -> Element {
     // import futures::StreamExt to use the next() method
     use futures::StreamExt;
     let mut response_state = use_signal(|| None);
-    let tx = use_coroutine(move |mut rx| {
-        async move {
-            // Define your state before the loop
-            let mut state = reqwest::Client::new();
-            let mut cache: HashMap<String, String> = HashMap::new();
-            loop {
-                // Loop and wait for the next message
-                if let Some(request) = rx.next().await {
-                    // Resolve the message
-                    let response = if let Some(response) = cache.get(&request) {
-                        response.clone()
-                    } else {
-                        let response = state
-                            .get(&request)
-                            .send()
-                            .await
-                            .unwrap()
-                            .text()
-                            .await
-                            .unwrap();
-                        cache.insert(request, response.clone());
-                        response
-                    };
-                    response_state.set(Some(response));
+    let tx = use_coroutine(move |mut rx| async move {
+        // Define your state before the loop
+        let mut state = reqwest::Client::new();
+        let mut cache: HashMap<String, String> = HashMap::new();
+        loop {
+            // Loop and wait for the next message
+            if let Some(request) = rx.next().await {
+                // Resolve the message
+                let response = if let Some(response) = cache.get(&request) {
+                    response.clone()
                 } else {
-                    break;
-                }
+                    let response = state
+                        .get(&request)
+                        .send()
+                        .await
+                        .unwrap()
+                        .text()
+                        .await
+                        .unwrap();
+                    cache.insert(request, response.clone());
+                    response
+                };
+                response_state.set(Some(response));
+            } else {
+                break;
             }
         }
     });

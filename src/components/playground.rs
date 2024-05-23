205143fa2440d9fcdbe905f2d5cae8f849734f76
Miles Murgaw <milesmurgaw@gmail.com>
2024-05-23 13:23:48 -0400

    revision: upgrade to edition 2024

--- src/components/playground.rs
+++ src/components/playground.rs
@@ -1,11 +1,23 @@
 use dioxus::prelude::*;
 
+#[cfg(not(debug_assertions))]
+const SOCKET_URI: &str = "ws://play.dioxuslabs.com/ws";
+
+#[cfg(debug_assertions)]
+const SOCKET_URI: &str = "ws://localhost:3000/ws";
+
+#[cfg(not(debug_assertions))]
+const BUILT_URI: &str = "https://play.dioxuslabs.com/built/";
+
+#[cfg(debug_assertions)]
+const BUILT_URI: &str = "http://localhost:3000/built/";
+
 #[component]
 pub fn Playground() -> Element {
     rsx! {
         dioxus_playground::Playground {
-            socket_uri: "ws://localhost:3000/ws".to_string(),
-            built_uri: "http://localhost:3000/built/".to_string(),
+            socket_uri: SOCKET_URI,
+            built_uri: BUILT_URI,
         },
     }
 }

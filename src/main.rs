05b9c48d91e738099d7bf3fb2996257886fc1323
Evan Almloff <evanalmloff@gmail.com>
2024-09-09 11:09:59 -0500

    Fix CodeBlock

--- src/main.rs
+++ src/main.rs
@@ -132,6 +132,23 @@ mod docs {
     use dioxus::prelude::*;
 
     #[component]
+    fn CodeBlock(contents: String) -> Element {
+        rsx! {
+            div {
+                style: "position: relative;",
+                div {
+                    dangerous_inner_html: contents
+                }
+                button {
+                    style: "position: absolute; top: 0; right: 0; background: rgba(0, 0, 0, 0.75); color: white; border: 1px solid white; padding: 0.25em;",
+                    "onclick": "navigator.clipboard.writeText(this.previousElementSibling.innerText)",
+                    "Copy"
+                }
+            }
+        }
+    }
+
+    #[component]
     fn SandBoxFrame(url: String) -> Element {
         rsx! {
             iframe {

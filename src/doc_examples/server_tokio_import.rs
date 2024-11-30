f44057bfeaf2b9c9279b2f16425eaec616b35a84
Andrew Voynov <andrewvoynov.b@gmail.com>
2024-11-30 19:36:27 +0300

    docs(server_tokio_import): added server function

--- src/doc_examples/server_tokio_import.rs
+++ src/doc_examples/server_tokio_import.rs
@@ -10,7 +10,8 @@ use tokio::io::AsyncReadExt;
 
 // ANCHOR: tokio_usage
 // Since the tokio dependency is only enabled in the server feature,
-// we need to only compile any usage of the dependency when the server feature is enabled.
+// we need to only compile any usage of the dependency when the server feature
+// is enabled.
 #[cfg(feature = "server")]
 async fn read_file() -> Result<String, std::io::Error> {
     let mut file = File::open("path/to/file").await?;
@@ -19,7 +20,8 @@ async fn read_file() -> Result<String, std::io::Error> {
     Ok(contents)
 }
 
-// The bodies of server functions automatically only compile when the server feature is enabled.
+// The bodies of server functions automatically only compile when the server
+// feature is enabled.
 #[server]
 async fn get_file_contents() -> Result<String, ServerFnError> {
     let mut file = File::open("path/to/file").await?;
@@ -30,8 +32,9 @@ async fn get_file_contents() -> Result<String, ServerFnError> {
 // ANCHOR_END: tokio_usage
 
 // ANCHOR: tokio_module
-// Instead of configuring each item that is only used in the server, you can group
-// them into a module that is only compiled when the server feature is enabled.
+// Instead of configuring each item that is only used in the server, you can
+// group them into a module that is only compiled when the server feature
+// is enabled.
 #[cfg(feature = "server")]
 mod tokio_utilities {
     use tokio::fs::File;
@@ -43,5 +46,12 @@ mod tokio_utilities {
         file.read_to_string(&mut contents).await?;
         Ok(contents)
     }
+}
+
+// Now you can define a slim server function (wrapper) that is accessible by the
+// client.
+#[server]
+async fn get_file_contents() -> Result<String, ServerFnError> {
+    tokio_utilities::read_file().await
 }
 // ANCHOR_END: tokio_module

7a574329c4e038d002706747e709e5c2620a4500
Evan Almloff <evanalmloff@gmail.com>
2025-01-31 14:39:27 -0600

    remove once logic from generate_search_index

--- src/components/search.rs
+++ src/components/search.rs
@@ -4,14 +4,10 @@ pub fn generate_search_index() {
         use crate::{static_dir, Route};
         use std::sync::atomic::{AtomicBool, Ordering};
 
-        static INDEX_FRESH: AtomicBool = AtomicBool::new(false);
-        let index_fresh = INDEX_FRESH.swap(true, Ordering::Relaxed);
-        if !index_fresh {
-            std::env::set_var("CARGO_MANIFEST_DIR", static_dir().join("assets"));
-            dioxus_search::SearchIndex::<Route>::create(
-                "searchable",
-                dioxus_search::BaseDirectoryMapping::new(static_dir()),
-            );
-        }
+        std::env::set_var("CARGO_MANIFEST_DIR", static_dir().join("assets"));
+        dioxus_search::SearchIndex::<Route>::create(
+            "searchable",
+            dioxus_search::BaseDirectoryMapping::new(static_dir()),
+        );
     }
 }

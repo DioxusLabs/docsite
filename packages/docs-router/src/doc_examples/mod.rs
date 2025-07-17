0768ba8363e0bc41928cc6dedf884ace58bd0f1f
Jonathan Kelley <jkelleyrtp@gmail.com>
2025-07-17 16:01:39 -0700

    reshuffle docs around again

---
+++ packages/docs-router/src/doc_examples/mod.rs
@@ -0,0 +1,71 @@
+pub use crate::{DemoFrame, SandBoxFrame, ComponentWithLogs, FakePage, CodeBlock, log, LogState, TwoPanelComponent};
+
+// Include any examples we compile into the docsite
+#[cfg(not(feature = "doc_test"))]
+pub mod __interactive_04;
+#[cfg(not(feature = "doc_test"))]
+pub mod asynchronous;
+#[cfg(not(feature = "doc_test"))]
+pub mod boolean_attribute;
+#[cfg(not(feature = "doc_test"))]
+pub mod breaking_out;
+#[cfg(not(feature = "doc_test"))]
+pub mod building_uis_with_rsx;
+#[cfg(not(feature = "doc_test"))]
+pub mod component_children;
+#[cfg(not(feature = "doc_test"))]
+pub mod component_lifecycle;
+#[cfg(not(feature = "doc_test"))]
+pub mod component_owned_props;
+#[cfg(not(feature = "doc_test"))]
+pub mod components;
+#[cfg(not(feature = "doc_test"))]
+pub mod conditional_rendering;
+#[cfg(not(feature = "doc_test"))]
+pub mod dangerous_inner_html;
+#[cfg(not(feature = "doc_test"))]
+pub mod error_handling;
+#[cfg(not(feature = "doc_test"))]
+pub mod event_click;
+#[cfg(not(feature = "doc_test"))]
+pub mod event_prevent_default;
+#[cfg(not(feature = "doc_test"))]
+pub mod full_example;
+#[cfg(not(feature = "doc_test"))]
+pub mod hackernews_async;
+#[cfg(not(feature = "doc_test"))]
+pub mod hackernews_complete;
+#[cfg(not(feature = "doc_test"))]
+pub mod hackernews_post;
+#[cfg(not(feature = "doc_test"))]
+pub mod hackernews_state;
+#[cfg(not(feature = "doc_test"))]
+pub mod hello_world;
+#[cfg(not(feature = "doc_test"))]
+pub mod hooks_counter;
+#[cfg(not(feature = "doc_test"))]
+pub mod hooks_counter_two_state;
+#[cfg(not(feature = "doc_test"))]
+pub mod hooks_use_signal;
+#[cfg(not(feature = "doc_test"))]
+pub mod input_controlled;
+#[cfg(not(feature = "doc_test"))]
+pub mod input_uncontrolled;
+#[cfg(not(feature = "doc_test"))]
+pub mod moving_state_around;
+#[cfg(not(feature = "doc_test"))]
+pub mod reactivity;
+#[cfg(not(feature = "doc_test"))]
+pub mod readme;
+#[cfg(not(feature = "doc_test"))]
+pub mod rendering_lists;
+#[cfg(not(feature = "doc_test"))]
+pub mod rsx_overview;
+#[cfg(not(feature = "doc_test"))]
+pub mod spawn;
+#[cfg(not(feature = "doc_test"))]
+pub mod use_resource;
+
+// Check any examples we don't compile into the docs
+#[cfg(feature = "doc_test")]
+automod::dir!(pub "src/doc_examples");
--- packages/docs-router/src/lib.rs
+++ packages/docs-router/src/lib.rs
@@ -2,6 +2,9 @@
 
 use dioxus::prelude::*;
 
+pub mod doc_examples;
+pub use doc_examples::*;
+
 mod desktop_dependencies;
 pub use desktop_dependencies::DesktopDependencies;
 
@@ -16,11 +19,11 @@ pub fn TwoPanelComponent(left: Element, right: Element) -> Element {
 }
 
 #[derive(Default)]
-struct LogState {
-    logs: Vec<String>,
+pub struct LogState {
+    pub logs: Vec<String>,
 }
 
-fn use_provide_log_state() -> Signal<LogState> {
+pub fn use_provide_log_state() -> Signal<LogState> {
     use_context_provider(|| Signal::new(LogState::default()))
 }
 
@@ -166,73 +169,3 @@ pub(crate) static Copy: Component<()> = |_| {
         }
     )
 };
-
-// Include any examples we compile into the docsite
-#[cfg(not(feature = "doc_test"))]
-pub mod __interactive_04;
-#[cfg(not(feature = "doc_test"))]
-pub mod asynchronous;
-#[cfg(not(feature = "doc_test"))]
-pub mod boolean_attribute;
-#[cfg(not(feature = "doc_test"))]
-pub mod breaking_out;
-#[cfg(not(feature = "doc_test"))]
-pub mod building_uis_with_rsx;
-#[cfg(not(feature = "doc_test"))]
-pub mod component_children;
-#[cfg(not(feature = "doc_test"))]
-pub mod component_lifecycle;
-#[cfg(not(feature = "doc_test"))]
-pub mod component_owned_props;
-#[cfg(not(feature = "doc_test"))]
-pub mod components;
-#[cfg(not(feature = "doc_test"))]
-pub mod conditional_rendering;
-#[cfg(not(feature = "doc_test"))]
-pub mod dangerous_inner_html;
-#[cfg(not(feature = "doc_test"))]
-pub mod error_handling;
-#[cfg(not(feature = "doc_test"))]
-pub mod event_click;
-#[cfg(not(feature = "doc_test"))]
-pub mod event_prevent_default;
-#[cfg(not(feature = "doc_test"))]
-pub mod full_example;
-#[cfg(not(feature = "doc_test"))]
-pub mod hackernews_async;
-#[cfg(not(feature = "doc_test"))]
-pub mod hackernews_complete;
-#[cfg(not(feature = "doc_test"))]
-pub mod hackernews_post;
-#[cfg(not(feature = "doc_test"))]
-pub mod hackernews_state;
-#[cfg(not(feature = "doc_test"))]
-pub mod hello_world;
-#[cfg(not(feature = "doc_test"))]
-pub mod hooks_counter;
-#[cfg(not(feature = "doc_test"))]
-pub mod hooks_counter_two_state;
-#[cfg(not(feature = "doc_test"))]
-pub mod hooks_use_signal;
-#[cfg(not(feature = "doc_test"))]
-pub mod input_controlled;
-#[cfg(not(feature = "doc_test"))]
-pub mod input_uncontrolled;
-#[cfg(not(feature = "doc_test"))]
-pub mod moving_state_around;
-#[cfg(not(feature = "doc_test"))]
-pub mod reactivity;
-#[cfg(not(feature = "doc_test"))]
-pub mod readme;
-#[cfg(not(feature = "doc_test"))]
-pub mod rendering_lists;
-#[cfg(not(feature = "doc_test"))]
-pub mod rsx_overview;
-#[cfg(not(feature = "doc_test"))]
-pub mod spawn;
-#[cfg(not(feature = "doc_test"))]
-pub mod use_resource;
-
-// Check any examples we don't compile into the docs
-#[cfg(feature = "doc_test")]
-automod::dir!(pub "src/doc_examples");

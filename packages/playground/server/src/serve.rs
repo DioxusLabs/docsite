28dc156472a46918817b1419790f7d1aec367132
Evan Almloff <evanalmloff@gmail.com>
2025-09-23 13:39:10 -0500

    use tower instead or rebuilding our own file server

--- packages/playground/server/src/serve.rs
+++ packages/playground/server/src/serve.rs
@@ -7,6 +7,8 @@ use axum::{
 use dioxus_logger::tracing::warn;
 use std::path::PathBuf;
 use tokio_util::io::ReaderStream;
+use tower_http::services::{ServeDir, ServeFile};
+use tower_util::ServiceExt;
 use uuid::Uuid;
 
 use crate::app::AppState;
@@ -16,29 +18,25 @@ use crate::app::AppState;
 pub async fn serve_built_index(
     State(state): State<AppState>,
     Path(build_id): Path<Uuid>,
+    request: axum::extract::Request,
 ) -> impl IntoResponse {
     let path = state.env.built_path.join(build_id.to_string());
 
     let index_path = path.join("index.html");
-    let file = match tokio::fs::File::open(index_path.clone()).await {
-        Ok(f) => f,
-        Err(e) => {
-            warn!(err = ?e, path = ?index_path, "failed to read built project:");
-            return Err((StatusCode::NOT_FOUND, "not found"));
-        }
-    };
 
-    let stream = ReaderStream::new(file);
-    let body = Body::from_stream(stream);
-
-    let headers = [(header::CONTENT_TYPE, "text/html")];
-
-    Ok((headers, body))
+    tower_http::services::ServeFile::new(index_path)
+        .oneshot(request)
+        .await
+        .map_err(|e| {
+            warn!(err = ?e, build_id = ?build_id, "failed to serve built project file:");
+            (StatusCode::NOT_FOUND, "not found")
+        })
 }
 
 pub async fn serve_other_built(
     State(state): State<AppState>,
     Path((build_id, file_path)): Path<(Uuid, PathBuf)>,
+    request: axum::extract::Request,
 ) -> impl IntoResponse {
     let path = state
         .env
@@ -46,36 +44,11 @@ pub async fn serve_other_built(
         .join(build_id.to_string())
         .join(file_path);
 
-    let file = match tokio::fs::File::open(path.clone()).await {
-        Ok(f) => f,
-        Err(e) => {
-            warn!(err = ?e, path = ?path, "failed to read built project:");
-            return Err((StatusCode::NOT_FOUND, "read failure"));
-        }
-    };
-
-    let Some(file_ext) = path.extension() else {
-        warn!(build_id = ?build_id, path = ?path, "failed to get file extension");
-        return Err((StatusCode::INTERNAL_SERVER_ERROR, "read failure"));
-    };
-
-    let content_type = match file_ext.to_str() {
-        Some("wasm") => "application/wasm",
-        Some("js") => "application/javascript",
-        Some(_) => {
-            warn!(build_id = ?build_id, path = ?path, "project tried accessing denied file");
-            return Err((StatusCode::NOT_FOUND, "not found"));
-        }
-        None => {
-            warn!(build_id = ?build_id, path = ?path, "failed to get file extension");
-            return Err((StatusCode::INTERNAL_SERVER_ERROR, "read failure"));
-        }
-    };
-
-    let stream = ReaderStream::new(file);
-    let body = Body::from_stream(stream);
-
-    let headers = [(header::CONTENT_TYPE, content_type)];
-
-    Ok((headers, body))
+    tower_http::services::ServeFile::new(path)
+        .oneshot(request)
+        .await
+        .map_err(|e| {
+            warn!(err = ?e, build_id = ?build_id, "failed to serve built project file:");
+            (StatusCode::NOT_FOUND, "not found")
+        })
 }
